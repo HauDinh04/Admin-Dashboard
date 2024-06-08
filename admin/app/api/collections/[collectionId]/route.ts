21e1543da4c6865f60f2b9d849bf2a2ed5d54819
Hau Dinh <Haudreamer2104@gmail.com>
2024-06-09 06:11:33 +0700

    2:30 code with Phuc

--- admin/app/api/collections/[collectionId]/route.ts
+++ admin/app/api/collections/[collectionId]/route.ts
@@ -3,18 +3,74 @@ import { auth } from "@clerk/nextjs/server";
 import Collection from "@/lib/models/Collection";
 import { NextRequest, NextResponse } from "next/server";
 
-export const DELETE = async (req:NextRequest,{params}:{params:{collectionId:string}})=>{
-    try{
-        const {userId}=auth()
-        if(!userId){
-            return new NextResponse("unauthorized",{status:401})
-        }
-        await connectToDB()
-        await Collection.findByIdAndDelete(params.collectionId)
-        return new NextResponse("Danh mục đã được xóa",{status:200})
-
-    }catch(err){
-        console.log("[Collections_Delete]",err)
-        return new NextResponse("Lỗi",{status:500})
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    await connectToDB();
+    const collection = await Collection.findById(params.collectionId);
+    if (!collection) {
+      return new NextResponse(JSON.stringify({ message: "Không tìm thấy" }), {
+        status: 404,
+      });
     }
-}
+    return NextResponse.json(collection, { status: 200 });
+  } catch (err) {
+    console.error("[collections GET]", err);
+    return new NextResponse("Lỗi", { status: 500 });
+  }
+};
+
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Không được phép", { status: 401 });
+    }
+
+    await connectToDB();
+    let collection = await Collection.findById(params.collectionId);
+    if (!collection) {
+      return new NextResponse("Không tìm thấy", { status: 404 });
+    }
+
+    const { title, description, image } = await req.json();
+    if (!title || !image) {
+      return new NextResponse("Tiêu đề và hình ảnh là bắt buộc", { status: 400 });
+    }
+
+    collection = await Collection.findByIdAndUpdate(
+      params.collectionId,
+      { title, description, image },
+      { new: true }
+    );
+    await collection.save();
+    return NextResponse.json(collection, { status: 200 });
+  } catch (err) {
+    console.error("[Collections_POST]", err);
+    return new NextResponse("Lỗi", { status: 500 });
+  }
+};
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { collectionId: string } }
+) => {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Không được phép", { status: 401 });
+    }
+
+    await connectToDB();
+    await Collection.findByIdAndDelete(params.collectionId);
+    return new NextResponse("Danh mục đã được xóa", { status: 200 });
+  } catch (err) {
+    console.error("[Collections_DELETE]", err);
+    return new NextResponse("Lỗi", { status: 500 });
+  }
+};
--- admin/components/collections/CollectionsForm.tsx
+++ admin/components/collections/CollectionsForm.tsx
@@ -16,38 +16,47 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "../ui/textarea";
 import ImageUpload from "../custom ui/ImageUpload";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
+import Delete from "../custom ui/Delete";
 
 const formSchema = z.object({
   title: z.string().min(2).max(20),
   description: z.string().min(2).max(500).trim(),
   image: z.string(),
 });
-
-const CollectionsForm = () => {
+interface CollectionFormProps {
+  initialData?: CollectionType | null;
+}
+const CollectionsForm: React.FC<CollectionFormProps> = ({ initialData }) => {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      description: "",
-      image: "",
-    },
+    defaultValues: initialData
+      ? initialData
+      : {
+          title: "",
+          description: "",
+          image: "",
+        },
   });
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
-      const res = await fetch("/api/collections", {
+      const url = initialData
+        ? `/api/collections/${initialData._id}`
+        : "/api/collections";
+      const res = await fetch(url, {
         method: "POST",
         body: JSON.stringify(values),
       });
       if (res.ok) {
         setLoading(false);
-        toast.success("danh mục đã được tạo");
+        toast.success(`danh mục ${initialData ? "cập nhật" : "tạo"}`);
+        window.location.href = "/collections";
         router.push("/collections");
       }
     } catch (err) {
@@ -57,7 +66,15 @@ const CollectionsForm = () => {
   };
   return (
     <div className="p-10">
-      <p className="text-heading2-bold">Tạo Mới Danh Mục</p>
+      {initialData ? (
+        <div className="flex items-center justify-between">
+          <p className="text-heading2-bold">Sửa Danh Mục</p>
+          <Delete id={initialData._id}/>
+        </div>
+      ) : (
+        <p className="text-heading2-bold">Tạo Mới Danh Mục</p>
+      )}
+
       <Separator className="bg-grey-1 mt-4 mb-7" />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
